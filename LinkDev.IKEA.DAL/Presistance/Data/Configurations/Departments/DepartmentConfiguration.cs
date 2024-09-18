442f3d895d5652ec533c8f4bfe6f86c9966d91a4
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-09-18 21:51:16 +0300

    Department Module (Entities, Configrations) and DbContext

---
+++ LinkDev.IKEA.DAL/Presistance/Data/Configurations/Departments/DepartmentConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using LinkDev.IKEA.DAL.Entities.Department;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Presistance.Data.Configurations.Departments
+{
+    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+
+            builder.Property(D => D.Name).HasColumnType("Varchar(50)").IsRequired();
+
+            builder.Property(D => D.Code).HasColumnType("Varchar(20)").IsRequired();
+
+            builder.Property(D => D.CreationDate).HasDefaultValueSql("GetDate()");
+
+            builder.Property(D => D.LastModifiedOn).HasComputedColumnSql("GetDate()");
+
+        }
+    }
+}
