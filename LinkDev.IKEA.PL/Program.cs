b2c9cf0136f61c9af8f5cdf06c2dcc270e9a8461
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-09-18 21:45:59 +0300

    MVC Project Structure

--- LinkDev.IKEA.PL/Program.cs
+++ LinkDev.IKEA.PL/Program.cs
@@ -7,7 +7,7 @@ namespace LinkDev.IKEA.PL
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews(); 
 
             var app = builder.Build();
 
