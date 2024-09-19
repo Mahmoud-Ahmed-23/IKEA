1a87bdf4f026766cca6b6e6d0f277f45e7198bd1
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-09-19 18:51:13 +0300

    Department Repository - DAL

---
+++ LinkDev.IKEA.DAL/Presistance/Reposatories/Departments/DepartmentRepositry.cs
@@ -0,0 +1,56 @@
+﻿
+using LinkDev.IKEA.DAL.Entities;
+using LinkDev.IKEA.DAL.Entities.Department;
+using LinkDev.IKEA.DAL.Presistance.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Presistance.Reposatories.Departments
+{
+    public class DepartmentRepositry : IDepartmentRepositry
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public DepartmentRepositry(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public Department? Get(int id)
+        {
+            return _dbcontext.Departments.Find(id);
+        }
+
+        public IEnumerable<Department> GetAll(bool AsNoTracking = true)
+        {
+            if (AsNoTracking)
+                return _dbcontext.Departments.ToList();
+
+            return _dbcontext.Departments.AsNoTracking().ToList();
+
+        }
+
+        public int Add(Department department)
+        {
+            _dbcontext.Departments.Add(department);
+            return _dbcontext.SaveChanges();
+        }
+
+        public int Update(Department department)
+        {
+            _dbcontext.Departments.Update(department);
+            return _dbcontext.SaveChanges();
+        }
+
+        public int Delete(Department department)
+        {
+            _dbcontext.Departments.Remove(department);
+            return _dbcontext.SaveChanges();
+        }
+
+    }
+}
---
+++ LinkDev.IKEA.DAL/Presistance/Reposatories/Departments/IDepartmentRepositry.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.IKEA.DAL.Entities.Department;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Presistance.Reposatories.Departments
+{
+	public interface IDepartmentRepositry
+	{
+		IEnumerable<Department> GetAll(bool AsNoTracking);
+		Department? Get(int id);
+		int Add(Department department);
+		int Update(Department department);
+		int Delete(Department department);
+
+	}
+}
