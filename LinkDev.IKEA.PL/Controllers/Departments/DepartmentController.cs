1ec53cd9aadffa33ec4ce08aceef3f737358f6ac
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-09-19 19:20:10 +0300

    Department Controller - Create

--- LinkDev.IKEA.PL/Controllers/Departments/DepartmentController.cs
+++ LinkDev.IKEA.PL/Controllers/Departments/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using LinkDev.IKEA.BLL.Services.Departments;
+﻿using LinkDev.IKEA.BLL.Models;
+using LinkDev.IKEA.BLL.Services.Departments;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LinkDev.IKEA.PL.Controllers.Department
@@ -7,17 +9,71 @@ namespace LinkDev.IKEA.PL.Controllers.Department
 	{
 
 		private readonly IDepartmentService _departmentService;
+        private readonly ILogger _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-		public DepartmentController(IDepartmentService departmentService)
-		{
-			_departmentService = departmentService;
-		}
 
-		[HttpGet]
+        public DepartmentController(IDepartmentService departmentService, ILogger logger, IWebHostEnvironment webHostEnvironment)
+        {
+            _departmentService = departmentService;
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
 		public IActionResult Index()
 		{
 			var deparments = _departmentService.GetAllDepartments();
 			return View(deparments);
 		}
-	}
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDto department)
+        {
+            if (!ModelState.IsValid)
+                return View(department);
+            var message = string.Empty;
+            try
+            {
+
+                var result = _departmentService.CreateDepartment(department);
+
+                if (result > 0)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    message = "Department is not Created";
+                    ModelState.AddModelError(string.Empty, message);
+                    return View(result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                if (_webHostEnvironment.IsDevelopment())
+                {
+                    message = ex.Message;
+                    return View(department);
+                }
+                else
+                {
+                    message = "Department is not Created";
+                    return View("Error", message);
+                }
+            }
+        }
+
+
+
+
+
+    }
 }
